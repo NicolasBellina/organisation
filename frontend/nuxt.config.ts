e2b712788d1300887f0ba0a5739f112d790a61cd
Nicolas Bellina <nicolasbellina72@gmail.com>
2025-03-21 10:38:12 +0100

    save

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -16,14 +16,9 @@ export default defineNuxtConfig({
     },
   },
 
-  runtimeConfig: {
-    public: {
-      supabaseUrl: process.env.SUPABASE_URL,
-      supabaseKey: process.env.SUPABASE_KEY
-    }
-  },
-
   supabase: {
+    url: 'https://gyqqifqlxygnmfwqxekl.supabase.co',
+    key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd5cXFpZnFseHlnbm1md3F4ZWtsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDIyMDM4NDgsImV4cCI6MjA1Nzc3OTg0OH0.BTsrAYLtxCz8bOGjSeDatD7nZVfVd14jY5-ZsGxIOB4',
     redirect: false
   },
 
